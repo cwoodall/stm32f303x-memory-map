34eb8dcd5f1f06d3c88703d84970bb2af140128d
Christopher J. Woodall <chris.j.woodall@gmail.com>
2017-03-31 19:09:54 -0400

    Added lines to take care of attribute formatting issues from svd2rust

--- gen-peripherals.sh
+++ gen-peripherals.sh
@@ -6,6 +6,8 @@ main() {
     local svd=resources/STM32F303x.svd
 
     svd2rust -i $svd > src/lib.rs
+    sed -i 's/\s*# \[ \([^]]*\) \]/\n#[\1]/g' src/lib.rs
+    sed -i 's/\s*# ! \[ \([^]]*\) \]/#![\1]\n/g' src/lib.rs
 
     set +e
     rustfmt src/*.rs
